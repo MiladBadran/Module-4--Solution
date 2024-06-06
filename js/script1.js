1f8315a9cdf4856ba7262ca9807e995444c231cc
MiladBadran <168366804+MiladBadran@users.noreply.github.com>
2024-06-06 09:19:48 -0900

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,14 @@
+
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // Loop over the names array and say either 'Hello' or 'Good Bye'
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})(window);
